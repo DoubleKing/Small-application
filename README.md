bf27a607c343d42536992b667cab6181b038c9e8
doubleking <971774262@qq.com>
2017-06-05 14:19:13 +0800

    app for json format 、url decode 、base64 decode

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Small application
+便于协议分析的小工具：包含JSON格式化、URL解码、Base64解码功能
+待优化、功能待扩充。
---
+++ myapp.py
@@ -0,0 +1,37 @@
+#coding:utf-8
+import wx
+import json
+import urllib
+import base64
+class myApp(wx.App):
+    def OnInit(self):
+        frame = wx.Frame(None,-1,'MY APP',pos = (50, 50),size = (1000, 1000),style = wx.DEFAULT_FRAME_STYLE,name = 'myApp')
+        panel = wx.Panel(frame, -1)
+        self.text1 = wx.TextCtrl(panel,-1,pos = (50, 50),size = (800, 400),style = wx.TE_RICH|wx.TE_MULTILINE)
+        self.text2 = wx.TextCtrl(panel,-1,pos = (50, 600),size = (800, 400),style = wx.TE_RICH|wx.TE_MULTILINE)
+        self.button1 = wx.Button(panel,-1,"JSON FORMAT",pos = (50, 520))
+        self.button2 = wx.Button(panel,-1,"URL DECODE",pos = (160, 520))
+        self.button3 = wx.Button(panel,-1,"BASE64 DECODE",pos = (270,520))
+        self.Bind(wx.EVT_BUTTON,self.OnButton1,self.button1)
+        self.Bind(wx.EVT_BUTTON,self.OnButton2,self.button2)
+        self.Bind(wx.EVT_BUTTON,self.OnButton3,self.button3)
+        frame.Show()
+        return True
+    def OnButton1(self, event):
+        try:
+            text = json.loads(self.text1.GetValue(),"unicode")
+            self.text2.SetValue(json.dumps(text,indent=4))
+        except:
+            self.text2.SetValue(u"\t\t\t\tcheck input")
+    def OnButton2(self, event):
+        try:
+            self.text2.SetValue(urllib.unquote(self.text1.GetValue().encode("ascii")).decode("utf-8"))
+        except:
+            self.text2.SetValue(u"\t\t\t\tcheck input")
+    def OnButton3(self, event):
+         try:
+           self.text2.SetValue(base64.b64decode(self.text1.GetValue().encode("ascii")).decode("utf-8"))
+         except:
+            self.text2.SetValue(u"\t\t\t\tcheck input")
+app = myApp()
+app.MainLoop()
